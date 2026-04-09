3a58d03625c29d6b921ba374bf9f5449ea21a6ba
Aline-Kelly <kelly.alinedasilva@gmail.com>
2026-04-08 22:03:16 -0300

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -2,5 +2,5 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from main import clima
 
-def test_clima_existe():  # Aline-Kelly
+def test_clima_existe():
     assert callable(clima)
